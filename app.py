e89d31cdf611221597e925320fb152542a99fb6d
Stephen Zou <zou.stephen@gmail.com>
2021-05-09 21:48:08 +1200

    add base template and error template

--- app.py
+++ app.py
@@ -4,25 +4,24 @@ import os
 
 app = Flask(__name__)
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
 @app.route('/')
 @app.route('/home')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
     return name
 
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello'))
-    print(url_for('user_page', name='greyli'))
-    print(url_for('user_page', name='peter'))
-    print(url_for('test_url_for'))
-    print(url_for('test_url_for', num=2))
-    return 'Test page'
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 # Config SQLite database
