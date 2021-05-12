ffb504de2d58ddca168be14479f977389995b38a
Stephen Zou <zou.stephen@gmail.com>
2021-05-12 21:38:31 +1200

    add user authentication with flask_login

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, url_for, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 import os
 
 app = Flask(__name__)
@@ -9,10 +11,23 @@ def inject_user():
     user = User.query.first()
     return dict(user=user)
 
+# Login management through flask_login
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     if request.method == 'POST':
+
+        if not current_user.is_authenticated:
+            return redirect(url_for('index'))
+        
         title = request.form.get('title')
         year = request.form.get('year')
 
@@ -30,6 +45,7 @@ def index():
     return render_template('index.html', movies=movies)
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+@login_required
 def edit(movie_id):
     movie = Movie.query.get_or_404(movie_id)
 
@@ -50,6 +66,7 @@ def edit(movie_id):
     return render_template('edit.html', movie=movie)
 
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
@@ -66,6 +83,36 @@ def user_page(name):
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+
+        if username == user.username and user.validate_password(password):
+            login_user(user)
+            flash('Login success')
+            return redirect(url_for('index'))
+
+        flash('Invalid username or password')
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Goodbye')
+    return redirect(url_for('index'))
+
 
 # Config SQLite database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db') 
@@ -76,9 +123,17 @@ db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'dev'
 
 # Database model
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password)  :
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
